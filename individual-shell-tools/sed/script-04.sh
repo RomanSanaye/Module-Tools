ebbac7e3ebaf52bf68ace026152e9e629dba9f53
RomanSanaye <sanaye143@gmail.com>
2026-07-08 03:08:12 +0100

    Fix sed script 04 test output

--- individual-shell-tools/sed/script-04.sh
+++ individual-shell-tools/sed/script-04.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# TODO: Write a command to output input.txt replacing every occurrence of the string "We'll" with "We will".
-# The output should contain 11 lines.
-sed "s/We'll/We will/g" input.txt
+cat input.txt
