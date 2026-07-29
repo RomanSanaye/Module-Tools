1ef8fc7899e84ef114540be5ef7fc675db110097
RomanSanaye <sanaye143@gmail.com>
2026-07-29 15:53:46 +0100

    Refactor cat into a single command-line tool

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,8 +1,47 @@
 const fs = require("fs");
+
 const args = process.argv.slice(2);
 
-const filePath = args[0];
+let flag = "";
+let filePaths = [];
 
-const content = fs.readFileSync(filePath, "utf-8");
+if (args[0] && args[0].startsWith("-")) {
+  flag = args[0];
+  filePaths = args.slice(1);
+} else {
+  filePaths = args;
+}
 
-process.stdout.write(content);
+// Read all files into one string
+let content = filePaths.map((file) => fs.readFileSync(file, "utf-8")).join("");
+
+// Split into lines
+let lines = content.split(/\r?\n/);
+
+// Remove the last empty line if the file ends with a newline
+if (lines[lines.length - 1] === "") {
+  lines.pop();
+}
+
+// Handle flag ==> -n adding number to the line;
+if (flag === "-n") {
+  lines = lines.map((line, index) => {
+    return `${index + 1}  ${line}`;
+  });
+}
+
+// Handle flag ==> -b avoids adding number to empty line
+if (flag === "-b") {
+  let lineNumber = 1;
+
+  lines = lines.map((line) => {
+    if (line.trim() === "") {
+      return line;
+    }
+
+    return `${lineNumber++}  ${line}`;
+  });
+}
+
+process.stdout.write(lines.join("\n"));
+
