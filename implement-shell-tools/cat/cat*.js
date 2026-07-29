54fe1c03292fec0bdecbb46bd76c89a7e494fbfa
RomanSanaye <sanaye143@gmail.com>
2026-07-29 21:14:15 +0100

    nothing special

--- implement-shell-tools/cat/cat*.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const folderPath = "./sample-files";
-const content = fs.readdirSync(folderPath);
-
-const txtFiles = content.filter((file) => file.endsWith(".txt"));
-process.stdout.write(txtFiles.join("\n"));
