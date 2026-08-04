a20739a8bde9bc42ee5855048b99bd7edbc7e588
RomanSanaye <sanaye143@gmail.com>
2026-08-04 21:33:12 +0100

    Imported the valid animal list from cowsay library

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -3,15 +3,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--animal", default="cow")
+parser.add_argument("--animal", default="cow", choices=cowsay.char_names)
 parser.add_argument("message", nargs="+")
 
 args = parser.parse_args()
 message = " ".join(args.message)
 
-if args.animal == "turtle":
-    cowsay.turtle(message)
-elif args.animal == "dragon":
-    cowsay.dragon(message)
-else:
-    cowsay.cow(message)
+# getting list of animals names from the cowsay library, using (char_names);
+animals = cowsay.char_names
+
+for animal in animals:
+    if args.animal == animal:
+        print(cowsay.get_output_string(animal, message))
+        break
